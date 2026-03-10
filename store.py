71ce867dbca5ec32cab3410dade1a1b0f2edfbee
MaxwellStu2023 <maxwell.tran@student.uts.edu.au>
2026-03-10 23:14:30 +1100

    10/03/2026 11:14pm

--- store.py
+++ store.py
@@ -1,15 +1,16 @@
 import sqlite3
-import pandas as pd
+from tabulate import tabulate
 
 # Initialise the Database and insert intial values
 
 con = sqlite3.connect("Store.db")
 cur = con.cursor()
 
-# cur.execute('CREATE TABLE items(id INTEGER PRIMARY KEY, name, price, stock, category)')
-# cur.execute('CREATE TABLE users(username, password, type)')
-#cur.execute(f"DROP TABLE IF EXISTS users") #delete entire table
-con.commit()
+#cur.execute('CREATE TABLE items(id INTEGER PRIMARY KEY, name, price, quantity, category)')
+#cur.execute('CREATE TABLE users(username, password, type)')
+cur.execute('CREATE TABLE userinventory(username, items, quantity)')
+#cur.execute(f"DROP TABLE IF EXISTS items") #delete entire table
+#con.commit()
 
 #cur.execute("""
 #     INSERT INTO users VALUES
@@ -19,16 +20,22 @@ con.commit()
 #con.commit()
 
 # cur.execute(""" 
-#     INSERT INTO items (name, price, stock) VALUES
-#         ('Apple', 2, 100, fruit),
-#         ('Banana', 1, 100, fruit),
-#         ('Pear', 3, 50, fruit),
-#         ('Steak', 20, 5, meat),
-#         ('Chicken Breast', 10, 20, meat),
-#         ('Pork Belly', 40, 2, meat)
+#     INSERT INTO items (name, price, quantity, category) VALUES
+#         ('Apple', 2, 100, 'fruit'),
+#         ('Banana', 1, 100, 'fruit'),
+#         ('Pear', 3, 50, 'fruit'),
+#         ('Steak', 20, 5, 'meat'),
+#         ('Chicken Breast', 10, 20, 'meat'),
+#         ('Pork Belly', 40, 2, 'meat')
 #     """)
 
-con.commit() #commit() method stores the entries into the disk 
+cur.execute("""
+            INSERT INTO userinventory (username, items, quantity) VALUES
+            (Chipsyyy, Apple, 5)
+            """
+)
+
+# con.commit() #commit() method stores the entries into the disk 
 
 # all = cur.execute("SELECT name, price FROM store ORDER BY price")
 # print(all.fetchall())
@@ -97,7 +104,6 @@ class Store:
                     u1.checkIfAdmin(username)
                     return False
 
-
     def deleteUser(self):
         delUsername = input("Which user do you want to delete? Enter their username: ")
         
@@ -111,34 +117,67 @@ class Store:
         pass
 
     def customerDashboard(self):
-            choice = input("""What would you like to do?
+            choice = int(input("""What would you like to do?
                                 1. Buy an item
                                 2. View invetory
                                 3. Return to home page
-                            """)
+                            """))
             
             if choice == 1: 
                 self.buy()
             elif choice == 2:
-                self.viewInventory()
+                self.previewItems()
             else:
                 main()
             pass
     
     def buy(self):
-        pass
+        df = pd.read_sql_query("SELECT * FROM items", con)
+        
+        print("\n--- Current Inventory ---")
+        print(tabulate(df, headers='keys', tablefmt='psql', showindex=False))
+        
+        while True:
+            itemID = input("What would you like to purchase? (Enter the ID number): ")
+            buyQuantity = int(input("How much of that item would you like to buy? "))
+            
+            itemQuantityQuery = "SELECT quantity FROM items where id = ?"
+            
+            cur.execute(itemQuantityQuery, (itemID,))
 
-    def viewInventory():
-        pass
+            itemQuantity = cur.fetchone()
+
+            if buyQuantity > itemQuantity[0]:
+                print("You cannot purchase more stock than what is available!")
+                continue
+            else:     
+                itemSearchQuery = """
+                                UPDATE items
+                                SET quantity = quantity - ?
+                                WHERE id = ?
+                                """
+                
+                cur.execute(itemSearchQuery, (buyQuantity, itemID))
+                con.commit()
+                cur.execute(f"SELECT name FROM items where id = {itemID}")
+                itemName = "".join(cur.fetchone())
+                if(itemName[-1] == "y" and buyQuantity > 1):
+                    itemsWithY = itemName[:-1] + "ies"
+                    print(f"You have bought {buyQuantity} {itemsWithY}")
+                elif(itemName[-1] == "y" and buyQuantity == 1):
+                    print(f"You have bought 1 {itemName}")
+                else:
+                    print(f"You have bought {buyQuantity} {itemName}s")
+                return False
     
     def adminPage(self):
-        choice = input("""What would you like to do?
+        choice = int(input("""What would you like to do?
                             1. Add an item
                             2. Restock an item
                             3. Remove an item
                             4. Return to home page
                             Choice: 
-                        """)
+                        """))
         
         if choice == 1: 
             self.addItem()
@@ -151,7 +190,27 @@ class Store:
         pass    
 
     def restock(self):
-        pass
+        self.previewItems()
+        itemID = input("What item do you want to restock? (Enter item ID): ")
+        restockQuantity = int(input("What do you want to set the stock value to?: "))
+        restockQuery = "SELECT quantity FROM items WHERE id = ?"
+
+        cur.execute(restockQuery, (itemID,))
+        cur.fetchone()
+
+        updateQuery = ("""
+                        UPDATE items
+                       SET quantity = ?
+                       WHERE id = ?                    
+                    """)
+
+        cur.execute(updateQuery, (restockQuantity, itemID))
+        con.commit()
+        cur.execute(f"SELECT name FROM items where id = {itemID}")
+        itemName = "".join(cur.fetchone())
+        print(f"You have set the stock value of {itemName} to {restockQuantity}")
+        print("\n+------+----------------+---------+------------+------------+")
+        self.customerDashboard()
 
     def addItem(self):
         itemCategory = input("Enter the item category (Fruit or Meat): ").lower()
@@ -173,15 +232,17 @@ class Store:
         cur.execute(removeQuery, itemName)
         con.commit()
         print(f"{itemName} has successfully been removed")
-        #print(pd.read_sql_query("SELECT * FROM items", con))
-        pass
 
-    def previewItems():
-        # all = cur.execute("SELECT * FROM items ORDER BY price")
-        # print(all.fetchall())
-        pass
+    def previewItems(self):
+        df = pd.read_sql_query("SELECT * FROM items", con)
+        
+        print("\n--- Current Inventory ---")
+        print(tabulate(df, headers='keys', tablefmt='psql', showindex=False))
 
 class User:
+    def __init__(self, name):
+        self.name = name
+
     def createStore(self):
         self.store = Store()
         return self.store
@@ -198,10 +259,11 @@ class User:
             self.createStore().customerDashboard()
         pass
 
+class Admin(User):
+    pass
 
-    def customerDashboard(self):
-
-        pass
+class Customer(User):
+    pass
 
 def main():
     s1 = Store()
